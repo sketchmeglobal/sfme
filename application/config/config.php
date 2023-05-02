d215f3673fb5070019e9638dfe7be364741c7cf8
sketchmeglobal <sketchmeglobal@gmail.com>
2023-05-02 21:14:48 +0530

    Config update check

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-
 // $config['base_url'] = 'http://localhost/sfme/';
 
 // $config['base_url'] = 'http://seafoodmiddleeast.net/';
