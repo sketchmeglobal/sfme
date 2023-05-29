3ea3870143a9b4ee3df5fc7e095609607cbbe4ec
sketchmeglobal <sketchmeglobal@gmail.com>
2023-05-29 10:10:04 +0530

    Update Offer_m.php

--- application/modules/admin_panel/models/Offer_m.php
+++ application/modules/admin_panel/models/Offer_m.php
@@ -2406,12 +2406,8 @@ class Offer_m extends CI_Model {
 
 
             /*-------Raw SQL-------*/
-
-            /*---------------------*/
-            echo "<pre>"; print_r($this->db->list_fields('exportdata'));
             echo $this->db->last_query();
-
-             echo "<pre>"; print_r($data['offer']);  die;
+            echo "<pre>"; print_r($this->db->list_fields('exportdata'));  die;
 
             $data['templates'] = $this->db->get_where('report_filter_template', array('report_filter_template_id' => $tepmlate_id))->row();
 
